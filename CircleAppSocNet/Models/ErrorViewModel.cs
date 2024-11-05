145d3c7f8788615b5ca3128281a3db6b97f8a8d2
ChevCellios <Korisnik@DESKTOP-I7ICCPO>
2024-11-05 11:53:52 +0100

    Add project files.

---
+++ CircleAppSocNet/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace CircleAppSocNet.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
